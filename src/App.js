b08af8854e6c9746cc88db75847f84a7e4c251f6
Jim Skerritt <jims@payscale.com>
2018-06-08 13:22:14 -0700

    add router components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <h1>Twilight Imperium 4th Edition</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <Router>
+                    <Route exact path="/" component={Home} />
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
