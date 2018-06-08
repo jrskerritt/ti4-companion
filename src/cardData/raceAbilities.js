765005681158e6660a8fd4b37c13e9cc8f327acd
RyanM <ryanm@payscale.com>
2018-06-08 14:59:14 -0700

    start of the race abilities

---
+++ src/cardData/raceAbilities.js
@@ -0,0 +1,20 @@
+const stallTactics = {
+    name: 'Stall Tactics',
+    value: 'ACTION: Discard 1 action card from your hand.'
+}
+
+const scheming = {
+    name: 'SCHEMING',
+    value: 'When you draw 1 or more action cards, draw 1 additional action card. Then choose and discard 1 action card from your hand.'
+}
+
+const crafty = {
+    name: 'CRAFTY',
+    value: 'You can have any number of action cards in your hand. Game effects cannot prevent you from using this ability.'
+}
+
+export const yssaril = {
+    0: stallTactics,
+    1: scheming,
+    2: crafty
+}
